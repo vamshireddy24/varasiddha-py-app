75e3141cb80bce8fc5f70248a152878310247b16
vamshireddy24 <88018641+vamshireddy24@users.noreply.github.com>
2024-09-25 21:06:35 +0530

    Update views.py

--- varasiddha/donations/views.py
+++ varasiddha/donations/views.py
@@ -6,8 +6,13 @@ from .models import Donation
 from django.http import FileResponse
 import os
 from django.conf import settings
+import logging
 
 def download_donation_info(request):
+    if request.user.is_authenticated:
+        logger.info(f"User {request.user.username} downloaded the donation info.")
+    else:
+        logger.info("An anonymous user downloaded the donation info.")
     file_path = os.path.join(settings.STATIC_ROOT, 'pdfs', 'donations.pdf')
     return FileResponse(open(file_path, 'rb'), content_type='application/pdf')
 def donations(request):
