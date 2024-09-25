85845b1b5074b4a61622e7bc0e5e682eade7ce35
vamshireddy24 <88018641+vamshireddy24@users.noreply.github.com>
2024-09-25 22:04:36 +0530

    Update settings.py

--- varasiddha/varasiddha/settings.py
+++ varasiddha/varasiddha/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 env = environ.Env()
-environ.Env.read_env(os.path.join(varasiddha, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
