ea513b8a1d06496173a60c9686d986b0623c6553
vamshireddy24 <88018641+vamshireddy24@users.noreply.github.com>
2024-09-25 22:23:19 +0530

    Update settings.py

--- varasiddha/varasiddha/settings.py
+++ varasiddha/varasiddha/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
 ROOT_URLCONF = 'varasiddha.urls'
